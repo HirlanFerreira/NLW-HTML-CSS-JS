956981c38932e33dbb2adf61b1da62ae5ff7be73
HirlanFerreira <hirlanreis17@gmail.com>
2024-02-11 23:52:08 -0300

    nova mudança

--- index.js
+++ index.js
@@ -91,24 +91,24 @@ const perguntas = [
   }
 ];
 
-const quiz = document.querySelector("quiz")
-const template = document.querySelector("template")
+const quiz = document.querySelector('#quiz')
+const template = document.querySelector('template')
 
 
 
 //loop ou laço de repetição
 for(const item of perguntas) {
   const quizItem = template.content.cloneNode(true)
-  quizItem.querySelector("h3").textContent = item.pergunta
+  quizItem.querySelector('h3').textContent = item.pergunta
 
   
   for(let resposta of item.respostas){
-    const dt = quizItem.querySelector("dl dt").cloneNode(true)
+    const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector("span").textContent = resposta
 
-    quizItem.querySelector("dl").appendChild(dt)
+    quizItem.querySelector('dl').appendChild(dt)
   }
-    quizItem.querySelector("dl dt").remove()
+    quizItem.querySelector('dl').remove()
 
   //coloca a pergunta na tela
   quiz.appendChild(quizItem)
